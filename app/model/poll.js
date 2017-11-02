30c92fd83b38562f7a5c17f4fc0878a30bbd59ac
aalokgupta <aalokgupta02@gmail.com>
2017-11-02 23:13:22 +0530

    model change

--- app/model/poll.js
+++ app/model/poll.js
@@ -3,10 +3,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Poll = new Schema({
-          poll: {poll_string: String,
-                 poll_option: Number,
-                 twitter_id: String,
-                }
-});
+var Poll = new Schema(
+                 { poll_string: String,
+                  poll_option1: String,
+                  poll_option2: String,
+                  poll_option3: String,
+                  poll_option4: String,
+                  twitter_id: String
+               });
+               
 module.exports = mongoose.model('Poll', Poll);
